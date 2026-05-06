f43ba06eee0aaaf92aaba504cb16313dd3b91e53
PrajwalS <prajwals@zktecodev.com>
2026-05-06 18:08:27 +0530

    added nginx.sh and app.conf

---
+++ nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo cp -rf app.conf /etc/nginx/conf.d
+chmod 710 /var/lib/jenkins/workspace/django_simple_CICD
+
+sudo nginx -t
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+echo "nginx has been started"
+
+sudo systemctl status nginx
