586d17dc59d17a276c6b202add4edbaef0e9e7cc
PrajwalS <prajwals@zktecodev.com>
2026-05-09 16:45:28 +0530

    Updated details in nginx

--- nginx.sh
+++ nginx.sh
@@ -12,3 +12,14 @@ sudo systemctl enable nginx
 echo "nginx has been started"
 
 sudo systemctl status nginx
+
+# -------------Explination-----------------
+#| Command                  | Purpose                     |
+#| ------------------------ | --------------------------- |
+#| `cp app.conf`            | install latest nginx config |
+#| `chmod 710`              | fix directory permissions   |
+#| `nginx -t`               | validate config             |
+#| `systemctl start nginx`  | start web server            |
+#| `systemctl enable nginx` | auto start on reboot        |
+#| `systemctl status nginx` | verify deployment           |
+
